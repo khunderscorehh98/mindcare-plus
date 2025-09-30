8fa8a527436cdf95b0eeec5e9c6d2481ffb43515
khunderscorehh98 <haziqimac@gmail.com>
2025-09-30 13:58:45 +0800

    App Vue Added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,55 +1,27 @@
+<!-- src/App.vue -->
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
+    <!-- Wrap every page in DefaultLayout -->
+    <DefaultLayout>
+      <router-view />
+    </DefaultLayout>
   </v-app>
 </template>
 
 <script>
+import DefaultLayout from './layouts/DefaultLayout.vue'
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  components: {
+    DefaultLayout,
+  },
+}
 </script>
+
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+</style>
